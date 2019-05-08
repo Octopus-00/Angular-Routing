210c333ba5c3d0db4c39420939ca2756cbf79096
onuroktay <ond@x-t.ch>
2019-05-08 04:17:05 +0200

    bootstrap config and routes define

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {RouterModule, Routes} from '@angular/router';
+import {PostsComponent} from './posts/posts.component';
+import {AboutComponent} from './about/about.component';
+import {NavbarComponent} from './navbar/navbar.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'posts',
+    component: PostsComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PostsComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
